97b65646a90c920dab9d48fd8531167b7a67a2fb
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-08-02 14:20:45 +0530

    Time: 7 ms (69.37%) | Memory: 44.2 MB (37.20%) - LeetSync

--- leetcode-solutions/567-permutation-in-string/permutation-in-string.java
+++ leetcode-solutions/567-permutation-in-string/permutation-in-string.java
@@ -1,38 +1,36 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()) return false;
-
+        
         int freq1[] = new int[26];
         int freq2[] = new int[26];
 
-        for(int i = 0; i < s1.length() ;i++){
-            freq1[s1.charAt(i) - 'a']++;
+
+        for(char c : s1.toCharArray()){
+            freq1[c - 'a']++; 
         }
 
         int left = 0;
 
-        for(int right = 0 ; right < s2.length() ; right++){
+        for(int right = 0; right < s2.length(); right++){
             freq2[s2.charAt(right) - 'a']++;
 
             if(right - left + 1 > s1.length()){
                 freq2[s2.charAt(left) - 'a']--;
                 left++;
             }
-
             if(right - left + 1 == s1.length()){
-                boolean isMatch = true;
-                for(int i = 0 ; i < 26 ; i++){
+                boolean ismatch = true;
+
+                for( int i = 0; i < 26 ; i++){
                     if(freq1[i] != freq2[i]){
-                        isMatch = false;
+                        ismatch = false;
                         break;
                     }
                 }
-                if(isMatch)
+                if(ismatch) 
                     return true;
             }
         }
-
         return false;
-
     }
 }
