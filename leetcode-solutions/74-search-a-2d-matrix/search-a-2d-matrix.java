65f0f6465b9d8720043cc21db857668966220586
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-08-16 19:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (24.58%) - LeetSync

--- leetcode-solutions/74-search-a-2d-matrix/search-a-2d-matrix.java
+++ leetcode-solutions/74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,22 +1,49 @@
 class Solution {
+
     public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m - 1;
+
         
-        int row = matrix.length;
-        int col = matrix[0].length;
+        while (left <= right) {
 
-        int i = 0;
-        int j = col - 1;
+            int mid = left + (right - left) / 2;
 
-        while (i < row && j >= 0) {
-
-            if (matrix[i][j] == target) {
-                return true;
+            if (target >= matrix[mid][0] && target <= matrix[mid][n - 1]) {
+                return binarySearch(matrix[mid], target);
             }
-            else if (matrix[i][j] > target) {
-                j--;
+            else if (target < matrix[mid][0]) {
+                right = mid - 1;
             }
             else {
-                i++;
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean binarySearch(int[] row, int target) {
+
+        int left = 0;
+        int right = row.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (row[mid] == target) {
+                return true;
+            }
+            else if (row[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
 
