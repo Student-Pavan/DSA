4cc7fa971aa668a6db4c703ce297e46d1294bbf0
Pavan Kumar <159564756+Student-Pavan@users.noreply.github.com>
2026-09-12 11:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (97.07%) - LeetSync

--- leetcode-solutions/198-house-robber/house-robber.java
+++ leetcode-solutions/198-house-robber/house-robber.java
@@ -1,18 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length ==0)
+            return 0;
 
-        if (nums.length == 1) {
+        if(nums.length == 1)
             return nums[0];
+
+        int case1 = canRob(nums,0,nums.length-1);
+        int case2 = canRob(nums,1,nums.length-1);
+
+        return Math.max(case1,case2);
+    }
+
+    private int canRob(int[] nums,int start, int end){
+        int prev1 = 0,prev2 =0;
+
+        for(int i = start; i <= end; i++){
+            int rob = nums[i] + prev2;
+            int curr = Math.max(rob,prev1);
+
+            prev2 = prev1;
+            prev1 = curr;
         }
-
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Integer.max(nums[0], nums[1]);
-
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Integer.max(dp[i - 1], nums[i] + dp[i - 2]);
-
-        }
-        return dp[nums.length - 1];
+        return prev1;
     }
 }
